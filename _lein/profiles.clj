83935966d4706549b9e7b2ee448eafa125b116f6
Ken Pu <kenpu@gmail.com>
2020-01-26 14:56:01 -0500

    lein profile

--- _lein/profiles.clj
+++ _lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.22.4"]
-                  [io.aviso/pretty "0.1.34"]]
+                  [io.aviso/pretty "0.1.34"]
+                  [venantius/ultra "0.6.0"]]
         :dependencies [[cljfmt "0.5.1"]
                        [io.aviso/pretty "0.1.34"]]}
  :repl-options {:init (require 'cljfmt.core)}}
